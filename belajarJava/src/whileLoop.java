3f44e076778643b63c2f6054a6823584e1e15fcc
RIVAL310490 <rivalalvianmelfandy@gmail.com>
2025-03-14 20:18:46 +0700

    Add files via upload

---
+++ belajarJava/src/whileLoop.java
@@ -0,0 +1,18 @@
+public class whileLoop {
+    public static void main(String[] args) {
+
+        /*
+        While Loop
+        * While loop adalah versi perulangan yang lebih sederhana dibanding for loop
+        * Di while loop, hanya terdapat kondisi perulangan, tanpa ada init statement dan post statement
+         */
+
+       var counter = 1;
+       while (counter <= 10) {
+           System.out.println("Aku sukses- " + counter);
+           counter++;
+       }
+
+
+    }
+}
